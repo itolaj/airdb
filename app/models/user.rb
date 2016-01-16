84ee3e2fdae1a589c45e304630212ad214c012a2
itolaj <itolaj@city.academic.gr>
2016-01-16 21:25:58 +0200

    Added attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :comments
 
   #uncomment this after deployent or localhost installation and after the first user has been signed up!
-  #validates :name, :expiration_date, :personal_number, :phone_number, :email, :password, :password_confirmation, :address, :user_role, presence: true
+  validates :name, :expiration_date, :personal_number, :phone_number, :email, :password, :password_confirmation, :address, :user_role, presence: true
 
   #Search action for users in users page
   def self.search(query)
