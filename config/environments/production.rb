82f59996c2c1f9c8cece3d4d0e01823209e71f2a
itolaj <itolaj@city.academic.gr>
2016-01-16 20:52:30 +0200

    Email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {:host => 'fury-airdb.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+ config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :authentication => :plain,   # I've also tried :login
+     :enable_starttls_auto => true,  # Also tried tls => true
+     :user_name => 'airdb@gmail.com',
+     :password => 'papertowns21'
+   }
 end
